e61e330c1bab49eaf1c340d4658d9621e876ba0b
vipul tyagi <vipultyagi840@gmail.com>
2023-10-17 17:14:50 +0530

    This is a Corousel

---
+++ script.js
@@ -0,0 +1,25 @@
+SlideIndex=1; 
+showSlides(SlideIndex);
+function plusSlides(n) {
+    showSlides(SlideIndex += n);
+  }
+  
+  // Thumbnail image controls
+function currentSlide(n){
+    showSlides(SlideIndex = n);
+}   
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {SlideIndex = 1}    
+    if (n < 1) {SlideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[SlideIndex-1].style.display = "block";  
+    dots[SlideIndex-1].className += " active";
+  }
